080c960b2b775ce25a2906946faa5fb34bb72062
dayo4 <dayo4@live.com>
2023-07-04 17:06:32 +0000

    Jul 4, 2023, 6:06 PM

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,7 +48,7 @@ export default defineNuxtConfig({
     },
   },
   plugins: [
-    // "~/plugins/navs/index.ts"
+    // 
   ],
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: [
