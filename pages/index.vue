5cae7ff032e58c4d8b33eae89e13a76c6977812a
dayo4 <dayo4@live.com>
2023-07-09 10:54:07 +0000

    Jul 9, 2023, 11:54 AM

--- pages/index.vue
+++ pages/index.vue
@@ -3,26 +3,31 @@
     <h1>Picet</h1>
 
     <section class="flex justify-center">
-    <div class="flex justify-center">
-      <div class="Section">
-        <h3 class="Head">Description</h3>
-        <span v-show="msg_err" class="Error t-red-1">{{ msg_err }}</span>
-        <div
-          ref="msgInput"
-          @input="setMsg"
-          class="TextArea bg-white font-5 br2 p-7 mt-2"
-          contenteditable="true"
-          placeholder="Type your message"
-        ></div>
-      </div>
+      <div class="flex justify-center">
+        <div class="Section">
+          <h3 class="Head">Description</h3>
+          <span v-show="msg_err" class="Error t-red-1">{{ msg_err }}</span>
+          <div
+            ref="msgInput"
+            @input="setMsg"
+            class="TextArea bg-white font-5 br2 p-7 mt-2"
+            contenteditable="true"
+            placeholder="Type your message"
+          ></div>
+        </div>
 
-      <!-- Send button -->
-      <div class="flex j-c-center my-8">
-        <Button @clicked="send" type="action" size="medium" icon="icon-forward">
-          Send {{ target === "app" ? "Mail" : "" }}
-        </Button>
+        <!-- Send button -->
+        <div class="flex j-c-center my-8">
+          <Button
+            @clicked="send"
+            type="action"
+            size="medium"
+            icon="icon-forward"
+          >
+            Send {{ target === "app" ? "Mail" : "" }}
+          </Button>
+        </div>
       </div>
-    </d>
     </section>
   </div>
 </template>
