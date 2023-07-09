56a46351b2e14864ecea9ccb3d78a8cdce1b4cf2
dayo4 <dayo4@live.com>
2023-07-09 10:25:34 +0000

    Jul 9, 2023, 11:25 AM

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Picet</h1>
 
-    <section>
+    <section class="flex">
       <div class="Section">
         <span class="Head">Description</span>
         <span v-show="msg_err" class="Error t-red-1">{{ msg_err }}</span>
