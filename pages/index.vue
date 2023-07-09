0405e02dcccbc1475200b9959af80f1c7f89291a
dayo4 <dayo4@live.com>
2023-07-09 10:08:18 +0000

    Jul 9, 2023, 11:08 AM

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
     <h1>Picet</h1>
+
+    <div class="Section">
+      <span class="Head">Your Message</span>
+      <span v-show="msg_err" class="Error t-red-1">{{ msg_err }}</span>
+      <div
+        ref="msgInput"
+        @input="setMsg"
+        class="TextArea bg-white font-5 br2 p-7 mt-2"
+        contenteditable="true"
+        placeholder="Type your message"
+      ></div>
+    </div>
+    
   </div>
 </template>
 <script lang="ts">
-
-import { $baseApi } from "@/utils/Axios"
+import { $baseApi } from "@/utils/Axios";
 
 export default {
   name: "Home",
@@ -19,6 +31,4 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
