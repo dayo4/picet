fb6cb775061b4ccfe25ff059c4782cf3f155637f
dayo4 <dayo4@live.com>
2023-07-09 10:21:35 +0000

    Jul 9, 2023, 11:21 AM

--- pages/index.vue
+++ pages/index.vue
@@ -2,30 +2,26 @@
   <div>
     <h1>Picet</h1>
 
-
-    <div class="Section">
-      <span class="Head">Description</span>
-      <span v-show="msg_err" class="Error t-red-1">{{ msg_err }}</span>
-      <div
-        ref="msgInput"
-        @input="setMsg"
-        class="TextArea bg-white font-5 br2 p-7 mt-2"
-        contenteditable="true"
-        placeholder="Type your message"
-      ></div>
-    </div>
+    <section>
+      <div class="Section">
+        <span class="Head">Description</span>
+        <span v-show="msg_err" class="Error t-red-1">{{ msg_err }}</span>
+        <div
+          ref="msgInput"
+          @input="setMsg"
+          class="TextArea bg-white font-5 br2 p-7 mt-2"
+          contenteditable="true"
+          placeholder="Type your message"
+        ></div>
+      </div>
 
       <!-- Send button -->
       <div class="flex j-c-center my-8">
-        <Button
-          @clicked="send"
-          type="action"
-          size="medium"
-          icon="icon-forward"
-        >
+        <Button @clicked="send" type="action" size="medium" icon="icon-forward">
           Send {{ target === "app" ? "Mail" : "" }}
         </Button>
       </div>
+    </section>
   </div>
 </template>
 <script lang="ts">
